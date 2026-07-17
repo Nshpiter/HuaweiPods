b4f28028a1d65db442ff9a9b3c76dcce35ef727b
Nshpiter <176191868+Nshpiter@users.noreply.github.com>
2026-07-17 16:48:28 +0800

    fix: prefer official Maven repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,27 +2,27 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
         maven("https://maven.aliyun.com/repository/gradle-plugin")
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/central")
         maven("https://maven.aliyun.com/repository/public")
-        gradlePluginPortal()
-        google()
-        mavenCentral()
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/central")
-        maven("https://maven.aliyun.com/repository/public")
         google()
         mavenCentral()
         maven("https://api.xposed.info/")
         maven("https://s01.oss.sonatype.org/content/repositories/releases/")
         maven("https://jitpack.io")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/public")
     }
 }
 
